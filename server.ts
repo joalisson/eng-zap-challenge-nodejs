83523c5c2d0e7fcbefc261a57ef8af647ad7c28c
Joalisson Andrade <joalisson.as@gmail.com>
2020-01-06 09:52:25 -0300

    Improve route

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import app from './app';
 import logger from './logger';
-import properties from './src/routes';
+import routes from './src/routes';
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/', properties);
+app.use('/', routes);
 
 app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
